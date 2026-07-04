52229dbaeeee15450dac4747bc20ac039691475d
Arturas Deltuvas <arturas.deltuvas@bentley.com>
2026-07-04 18:16:10 +0300

    Updates

---
+++ internal/helpers/data/numeric.go
@@ -0,0 +1,8 @@
+package data
+
+type Numeric interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
+		~float32 | ~float64 |
+		~complex64 | ~complex128
+}
---
+++ internal/helpers/data/vec2.go
@@ -0,0 +1,47 @@
+package data
+
+type Vec2[T Numeric] struct{ X, Y T }
+
+func NewVec2[T Numeric](x, y T) Vec2[T] { return Vec2[T]{X: x, Y: y} }
+
+func (this Vec2[T]) Add(other Vec2[T]) Vec2[T] {
+	this.X += other.X
+	this.Y += other.Y
+	return this
+}
+
+func (this Vec2[T]) Subtract(other Vec2[T]) Vec2[T] {
+	this.X -= other.X
+	this.Y -= other.Y
+	return this
+}
+
+func (this Vec2[T]) MultiplyComponent(other Vec2[T]) Vec2[T] {
+	this.X *= other.X
+	this.Y *= other.Y
+	return this
+}
+
+func (this Vec2[T]) MultiplyScalar(scalar T) Vec2[T] {
+	this.X *= scalar
+	this.Y *= scalar
+	return this
+}
+
+func (this Vec2[T]) DivideComponent(other Vec2[T]) Vec2[T] {
+	this.X /= other.X
+	this.Y /= other.Y
+	return this
+}
+
+func (this Vec2[T]) DivideScalar(scalar T) Vec2[T] {
+	this.X /= scalar
+	this.Y /= scalar
+	return this
+}
+
+func (this Vec2[T]) SquaredLength() T { return this.X*this.X + this.Y*this.Y }
+
+func (this Vec2[T]) DotProduct(other Vec2[T]) T { return this.X*other.X + this.Y*other.Y }
+
+func (this Vec2[T]) CrossProduct(other Vec2[T]) T { return this.X*other.Y - this.Y*other.X }
