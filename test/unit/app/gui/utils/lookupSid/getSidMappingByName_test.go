183f55493efd5b6d52b196cd6659757b42ac5049
Arturas Deltuvas <arturas.deltuvas@bentley.com>
2026-07-08 14:10:33 +0300

    Part 9.2

---
+++ test/unit/app/gui/utils/lookupSid/getSidMappingByName_test.go
@@ -0,0 +1,79 @@
+package lookupSid_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/Tariomka/hommoe_custom_templates/app/gui/constants"
+	"github.com/Tariomka/hommoe_custom_templates/app/gui/utils"
+	"github.com/Tariomka/hommoe_custom_templates/internal/models"
+	"github.com/brianvoe/gofakeit/v7"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWhenNameMatchesExactly_MappingIsReturned(t *testing.T) {
+	// Arrange
+	expected := constants.ContentIds.AlchemyLab
+
+	// Act
+	mapping, _ := utils.GetSidMappingByName(expected.Name)
+
+	// Assert
+	assert.Equal(t, expected, mapping)
+}
+
+func TestWhenNameMatchesExactly_LookupReportsFound(t *testing.T) {
+	// Arrange
+	name := constants.ContentIds.AlchemyLab.Name
+
+	// Act
+	_, found := utils.GetSidMappingByName(name)
+
+	// Assert
+	assert.True(t, found)
+}
+
+func TestWhenNameDiffersOnlyByCase_MappingIsReturned(t *testing.T) {
+	// Arrange
+	expected := constants.ContentIds.WiseOwl
+	upperCasedName := strings.ToUpper(expected.Name)
+
+	// Act
+	mapping, _ := utils.GetSidMappingByName(upperCasedName)
+
+	// Assert
+	assert.Equal(t, expected, mapping)
+}
+
+func TestWhenNameBelongsToIncludeList_MappingIsReturned(t *testing.T) {
+	// Arrange
+	expected := constants.IncludeListIds.RandomHiresLowTier
+
+	// Act
+	mapping, _ := utils.GetSidMappingByName(expected.Name)
+
+	// Assert
+	assert.Equal(t, expected, mapping)
+}
+
+func TestWhenNameIsUnknown_LookupReportsNotFound(t *testing.T) {
+	// Arrange
+	unknownName := gofakeit.UUID()
+
+	// Act
+	_, found := utils.GetSidMappingByName(unknownName)
+
+	// Assert
+	assert.False(t, found)
+}
+
+func TestWhenNameIsUnknown_ZeroMappingIsReturned(t *testing.T) {
+	// Arrange
+	unknownName := gofakeit.UUID()
+
+	// Act
+	mapping, _ := utils.GetSidMappingByName(unknownName)
+
+	// Assert
+	assert.Equal(t, models.SidMapping{}, mapping)
+}
---
+++ test/unit/app/gui/utils/lookupSid/getSidMappingBySid_test.go
@@ -0,0 +1,78 @@
+package lookupSid_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/Tariomka/hommoe_custom_templates/app/gui/constants"
+	"github.com/Tariomka/hommoe_custom_templates/app/gui/utils"
+	"github.com/Tariomka/hommoe_custom_templates/internal/models"
+	"github.com/brianvoe/gofakeit/v7"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWhenSidMatches_MappingIsReturned(t *testing.T) {
+	// Arrange
+	expected := constants.ContentIds.DragonUtopia
+
+	// Act
+	mapping, _ := utils.GetSidMappingBySid(expected.Sid)
+
+	// Assert
+	assert.Equal(t, expected, mapping)
+}
+
+func TestWhenSidMatches_LookupReportsFound(t *testing.T) {
+	// Arrange
+	sid := constants.ContentIds.DragonUtopia.Sid
+
+	// Act
+	_, found := utils.GetSidMappingBySid(sid)
+
+	// Assert
+	assert.True(t, found)
+}
+
+func TestWhenSidBelongsToIncludeList_MappingIsReturned(t *testing.T) {
+	// Arrange
+	expected := constants.IncludeListIds.ResourceBanksTier2
+
+	// Act
+	mapping, _ := utils.GetSidMappingBySid(expected.Sid)
+
+	// Assert
+	assert.Equal(t, expected, mapping)
+}
+
+func TestWhenSidCaseDiffers_LookupReportsNotFound(t *testing.T) {
+	// Arrange - sid comparison is case-sensitive, unlike the name lookup
+	upperCasedSid := strings.ToUpper(constants.ContentIds.DragonUtopia.Sid)
+
+	// Act
+	_, found := utils.GetSidMappingBySid(upperCasedSid)
+
+	// Assert
+	assert.False(t, found)
+}
+
+func TestWhenSidIsUnknown_LookupReportsNotFound(t *testing.T) {
+	// Arrange
+	unknownSid := gofakeit.UUID()
+
+	// Act
+	_, found := utils.GetSidMappingBySid(unknownSid)
+
+	// Assert
+	assert.False(t, found)
+}
+
+func TestWhenSidIsUnknown_ZeroMappingIsReturned(t *testing.T) {
+	// Arrange
+	unknownSid := gofakeit.UUID()
+
+	// Act
+	mapping, _ := utils.GetSidMappingBySid(unknownSid)
+
+	// Assert
+	assert.Equal(t, models.SidMapping{}, mapping)
+}
